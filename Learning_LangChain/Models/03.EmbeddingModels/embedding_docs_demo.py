b9fa16f71d81c73fa5baa9a3554b81cd2ea41271
manjot-baj <bajwa.mj78@gmail.com>
2026-09-17 19:05:38 +0530

    Embedding similarity semantic search

--- Learning_LangChain/Models/03.EmbeddingModels/embedding_docs_demo.py
+++ Learning_LangChain/Models/03.EmbeddingModels/embedding_docs_demo.py
@@ -8,7 +8,7 @@ embedding_model = OpenAIEmbeddings(model="text-embedding-3-large", dimensions=32
 documents = [
     "Mumbai is capital of Maharashtra"
     "Kolkata is capital of West Bengal"
-    "Ahmedabad is capital of Gujrat"
+    "Ahmedabad is capital of Gujarat"
 ]
 
 result = embedding_model.embed_documents(documents)
